25f1277059c3498f8c8abe0a2a9a31f9e7883fb1
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2022-10-03 17:29:53 +0700

    setup a new application using fastapi and postgres

---
+++ models.py
@@ -0,0 +1,45 @@
+
+import datetime
+
+from enum import auto
+from operator import index
+from xmlrpc.client import DateTime
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    items = relationship("Item", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")
+
+class Recipse(Base):
+  __tablename__ = "recipses"
+
+  id = Column(Integer, primary_key=True, index=True)
+  title = Column(String, index=True)
+  making_time = Column(String, index=True)
+  serves = Column(String, index=True)
+  ingredients = Column(String, index=True)
+  cost = Column(Integer, index=True)
+  created_at = Column(DateTime, index=True, default=datetime.datetime.now())
+  updated_at = Column(DateTime, index=True, onupdate=datetime.datetime.now())
+
---
+++ schemas.py
@@ -0,0 +1,71 @@
+from lib2to3.pytree import Base
+from typing import List, Union
+
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: Union[str, None] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
+
+# compact data of recipes
+class RecipesBase(BaseModel):
+  id: str
+  title: str
+  making_time: str
+  serves: str
+  ingredients: str
+  cost: str
+
+# base reponse for recipes
+class RecipesResponse(BaseModel):
+  message: str
+  recipes: Union[List[RecipesBase], None] = None
+
+# full data of recipes
+class RecipesBaseFull(RecipesBase):
+  created_at: str
+  updated_at: str
+
+# created response for recipes
+class RecipesCreateResponse(BaseModel):
+  message: str
+  recipes: Union[List[RecipesBaseFull], None] = None
+
+#List response for recipes
+class RecipesListResponse(BaseModel):
+  recipes: Union[List[RecipesBase], None] = None
+
+
+# delete response for recipes
+class RecipesDeleteResponse(BaseModel):
+  message: str
